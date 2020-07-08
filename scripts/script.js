e39b6b35e179b186b66e01428bd99df960272149
Nechitaylo Vladimir <vs_nechitaylo@bingo-boom.com>
2020-07-08 21:05:21 +0300

    form: Complete form

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,30 @@
 var popupElement = document.querySelector('.popup');
+var formElement = document.querySelector('.popup__form');
 
 var editButton = document.querySelector('.profile__edit-button');
 var closeButton = document.querySelector('.popup__close-button');
 
-function openPopup() {
-    popupElement.classList.add('popup_opened');
+function togglePopup() {
+  popupElement.classList.toggle('popup_opened');
 }
 
-function closePopup() {
-    popupElement.classList.remove('popup_opened');
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    togglePopup();
+
+    // Находим поля формы в DOM
+    let nameInput = document.querySelector('#name');
+    let jobInput =  document.querySelector('#job');
+
+    let profileTitle = document.querySelector('.profile__title');
+    let profileSubtitle = document.querySelector('.profile__subtitle');
+
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
