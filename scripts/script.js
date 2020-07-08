6908e141d3fd6eb2ba3725266b5aa3a906e4a56f
Nechitaylo Vladimir <vs_nechitaylo@bingo-boom.com>
2020-07-08 22:41:08 +0300

    script: fix popup value

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,20 @@
-var popupElement = document.querySelector('.popup');
-var formElement = document.querySelector('.popup__form');
+let popupElement = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
 
-var editButton = document.querySelector('.profile__edit-button');
-var closeButton = document.querySelector('.popup__close-button');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+
+let nameInput = document.querySelector('#name');
+let jobInput =  document.querySelector('#job');
+
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
 function togglePopup() {
   popupElement.classList.toggle('popup_opened');
+
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
 
 editButton.addEventListener('click', togglePopup);
@@ -14,17 +23,10 @@ closeButton.addEventListener('click', togglePopup);
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    togglePopup();
-
-    // Находим поля формы в DOM
-    let nameInput = document.querySelector('#name');
-    let jobInput =  document.querySelector('#job');
-
-    let profileTitle = document.querySelector('.profile__title');
-    let profileSubtitle = document.querySelector('.profile__subtitle');
-
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
+
+    togglePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
