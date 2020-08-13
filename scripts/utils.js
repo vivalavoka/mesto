c829edf712186dfe60309947f457a5429b7720f7
Nechitaylo Vladimir <vs_nechitaylo@bingo-boom.com>
2020-08-13 21:55:00 +0300

    hotfix: update modal window listeners

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,30 +4,21 @@ const _keyDownListener = (evt, popup) => {
   }
 }
 
-const _overlayClickListener = (evt, popup) => {
-  if (evt.currentTarget === evt.target) {
+const _closePopupListener = (evt, popup) => {
+  if (evt.target.classList.contains('popup__close-button') 
+    || evt.target.classList.contains('popup')) {
     closePopup(popup);
   }
 }
 
-const _closeButtonListener = (evt, popup) => {
-  closePopup(popup);
-}
-
 const _setPopupEventListeners = (popup) => {
-  const closeButton = popup.querySelector('.popup__close-button');
-
   document.addEventListener('keyup', (evt) => _keyDownListener(evt, popup));
-  popup.addEventListener('click', (evt) => _overlayClickListener(evt, popup));
-  closeButton.addEventListener('click', (evt) => _closeButtonListener(evt, popup));
+  popup.addEventListener('click', (evt) => _closePopupListener(evt, popup));
 }
 
 const _removePopupEventListeners = (popup) => {
-  const closeButton = popup.querySelector('.popup__close-button');
-
-  document.removeEventListener('keydown', (evt) => _keyDownListener(evt, popup));
-  popup.removeEventListener('click', (evt) => _overlayClickListener(evt, popup));
-  closeButton.removeEventListener('click', (evt) => _closeButtonListener(evt, popup));
+  document.removeEventListener('keyup', (evt) => _keyDownListener(evt, popup));
+  popup.removeEventListener('click', (evt) => _closePopupListener(evt, popup));
 }
 
 export const openPopup = (popup) => {
