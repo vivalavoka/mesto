23d9117c637193279c84701b5779cd8803488a58
Nechitaylo Vladimir <vs_nechitaylo@bingo-boom.com>
2020-08-31 23:26:56 +0300

    feature/components: Add babel config

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,18 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
+  },
+  module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        loader: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      }
+      ]
   }
 }
